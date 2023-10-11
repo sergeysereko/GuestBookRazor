e818b3e47e2a11ad1a0b05b9d5b3cd2c8b2a8716
sergey <sergeysereko@gmail.com>
2023-10-11 20:56:49 +0300

    task #3 Добавил модель Message

---
+++ GuestBookRazor/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuestBookRazor.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Текст сообщения: ")]
+        public string Message_text { get; set; }
+
+        public DateTime MessageDate { get; set; }
+
+        public int Id_User { get; set; }
+
+        public string? User { get; set; }
+    }
+}
