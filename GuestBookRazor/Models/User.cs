9c6b7c16c1ccdcb4600fbed038a74010fea3681e
sergey <sergeysereko@gmail.com>
2023-10-11 20:58:46 +0300

    task #4 Добавил модель User

---
+++ GuestBookRazor/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GuestBookRazor.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Имя пользователя: ")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Пароль: ")]
+        public string Pwd { get; set; }
+
+        public string? Salt { get; set; }
+    }
+}
