6a8eb609f521607d999278ae9898eb4966776820
sergey <sergeysereko@gmail.com>
2023-10-11 20:51:34 +0300

    task#1 Добавил модель Login

---
+++ GuestBookRazor/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿namespace GuestBookRazor.Models
+{
+    public class Login
+    {
+        //[Required(ErrorMessage = "Поле должно быть установлено")]
+        //[Display(Name = "Имя пользователя: ")]
+        public string UserName { get; set; }
+
+        //[Required(ErrorMessage = "Поле должно быть установлено")]
+        //[Display(Name = "Пароль: ")]
+        //[DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
