23417cc0da95b5f624102ebd403e7a88b5500fa6
sergey <sergeysereko@gmail.com>
2023-10-11 21:46:16 +0300

    task #6 Добавил репозиторий UserRepositoty Добавил контекст GuestBookContext

---
+++ GuestBookRazor/Models/GuestBookContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GuestBookRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuestBookRazor.Models
+{  
+    public class GuestBookContext : DbContext
+    {
+      public GuestBookContext(DbContextOptions<GuestBookContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+    
+}
+
+
---
+++ GuestBookRazor/Repository/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using GuestBookRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GuestBookRazor.Repository
+{
+    public class UserRepository : IRepository<User>
+    {
+        private GuestBookContext db;
+
+        public UserRepository(GuestBookContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            return await db.Users.ToListAsync();
+        }
+
+        public async Task<User> Get(string name)
+        {
+            User? user = await db.Users.FirstOrDefaultAsync(u => u.Name == name);
+            return user;
+        }
+
+        public async Task Create(User user)
+        {
+            await db.Users.AddAsync(user);
+        }
+
+        public async Task<User> Get(int id)
+        {
+            User? user = await db.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
