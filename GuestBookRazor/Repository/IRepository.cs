77a8b40f3441de48cf1b1f66f7d4d637bbde40df
sergey <sergeysereko@gmail.com>
2023-10-11 21:21:21 +0300

    task #5 Добавил интерфейс IRepository

---
+++ GuestBookRazor/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace GuestBookRazor.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll();
+        Task<T> Get(string name);
+        Task Create(T item);
+        Task<T> Get(int id);
+    }
+}
